0dea250265fcfe8ac5ba6e266986b702012e4ec0
Poof <poofy38@gmail.com>
2023-09-16 17:32:06 -0500

    Update

--- train.py
+++ train.py
@@ -10,6 +10,8 @@ import numpy as np
 import torch.nn.functional as F
 from torchvision import transforms
 from efficientnet_pytorch import EfficientNet
+import seaborn as sns
+from sklearn.metrics import confusion_matrix
 import torchvision.models as models
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from PIL import Image
@@ -123,7 +125,8 @@ class CASBUSI_Dataset(Dataset):
 
         has_unknown = same_patient_data.loc[0, 'Has_Unknown']
         if not has_unknown:
-            target = torch.tensor(same_patient_data.loc[0, ['Has_Malignant', 'Has_Benign']].values.astype('float'))
+            #target = torch.tensor(same_patient_data.loc[0, ['Has_Malignant', 'Has_Benign']].values.astype('float'))
+            target = torch.tensor(same_patient_data.loc[0, ['Has_Malignant']].values.astype('float'))
         else:
             target = None
 
@@ -141,16 +144,21 @@ def load_model(model, model_path):
 def collate_fn(batch):
     return batch
 
+
 def train_model(model, model_name, epochs):
     
-    # Define loss function and optimizer
     criterion = torch.nn.BCEWithLogitsLoss()
-    optimizer = torch.optim.Adam(model.parameters())
+    optimizer = torch.optim.Adam(model.parameters(), lr=.0001)
 
-    print_every = 100
-    
-    for epoch in range(epochs):  # Number of epochs
+    for epoch in range(epochs):
         model.train()
+        
+        train_true_labels = []
+        train_pred_labels = []
+        
+        val_true_labels = []
+        val_pred_labels = []
+        
         running_loss = 0.0
         running_corrects = 0
         running_total = 0
@@ -158,142 +166,114 @@ def train_model(model, model_name, epochs):
         final_loss = 0.0
         final_corrects = 0
         final_total = 0
-        for i, data in enumerate(train_loader):
-            bag, labels = data[0]  # Get the first item in the batch
-            if labels is None:  # Skip bags with label None
+
+        # Training loop
+        for i, data in tqdm(enumerate(train_loader), total=len(train_loader)):
+            bag, labels = data[0]
+            if labels is None:
                 continue
-            print(data)
             labels = labels.squeeze().to(device, non_blocking=True)
+            
+            train_true_labels.append(np.atleast_1d(labels.cpu().numpy()))  # Append true labels
 
             bag = bag.to(device, non_blocking=True)
-            
-            # Get the output for the entire bag in one forward pass
             outputs = model(bag).mean(dim=0).squeeze(0)
-
             loss = criterion(outputs, labels)
 
-            # Backward and optimize
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
+            predicted = torch.round(torch.sigmoid(outputs))
+            train_pred_labels.append(np.atleast_1d(predicted.detach().cpu().numpy()))  # Append predicted labels
+            
+            # Get Stats
             running_loss += loss.item()
             final_loss += loss.item()
-
-            # Calculate accuracy
             predicted = torch.round(torch.sigmoid(outputs))  # Convert to 0/1
-            running_total += labels.size(0)
-            running_corrects += (predicted == labels).sum().item()
-            final_total += labels.size(0)
+            final_total += 1 #labels.size(0)
             final_corrects += (predicted == labels).sum().item()
-
-            # Print every 'print_every' mini-batches
-            if i % print_every == (print_every-1):  # print every 'print_every' mini-batches
-                avg_loss = running_loss / print_every
-                avg_acc = running_corrects / running_total  # Multiply by label size to account for multiple labels per image
-                print(f'Epoch {epoch+1}, Step {i+1}/{len(train_loader)} - Loss: {avg_loss:.4f}, Acc: {avg_acc:.4f}')
-                running_loss = 0.0  # reset running loss
-                running_corrects = 0  # reset running corrects
-                running_total = 0  # reset total
-
         train_acc = final_corrects / final_total
-
+        
+        
+        # Validation loop
         model.eval()
-        val_running_loss = 0.0
         with torch.no_grad():
             val_running_loss = 0.0
             correct = 0
             total = 0
-
             for i, data in tqdm(enumerate(val_loader), total=len(val_loader)):
-                bag, labels = data[0]  # Get the first item in the batch
-                if labels is None:  # Skip bags with label None
+                bag, labels = data[0]
+                if labels is None:
                     continue
-
                 labels = labels.squeeze().to(device, non_blocking=True)
                 bag = bag.to(device, non_blocking=True)
-
-                # Get the output for the entire bag in one forward pass
                 outputs = model(bag).mean(dim=0).squeeze(0)
-
                 loss = criterion(outputs, labels)
+                
+                predicted = torch.round(torch.sigmoid(outputs))
+
+                val_true_labels.append(np.atleast_1d(labels.cpu().numpy()))
+                val_pred_labels.append(np.atleast_1d(predicted.cpu().numpy()))
+                
+                # Get Stats
                 val_running_loss += loss.item()
-
-                # Calculate accuracy
                 predicted = torch.round(torch.sigmoid(outputs))  # Convert to 0/1
-                total += labels.size(0)
+                total += 1 #labels.size(0)
                 correct += (predicted == labels).sum().item()
-
+                
             val_acc = correct / total if total > 0 else 0.0  # Prevent division by zero
             val_loss = val_running_loss / total if total > 0 else 0.0  # Prevent division by zero
 
-            print(f"Epoch {epoch+1} | Acc | Loss")
-            print(f"Train | {train_acc:.4f} | {final_loss / len(train_loader):.4f}")
-            print(f"Val | {val_acc:.4f} | {val_loss:.4f}")
-            
+        print(f"Epoch {epoch+1} | Acc   | Loss")
+        print(f"Train   | {train_acc:.4f} | {final_loss / len(train_loader):.4f}")
+        print(f"Val     | {val_acc:.4f} | {val_loss:.4f}")
+        
+        # Converting lists to numpy arrays for the epoch
+        train_true_labels = np.concatenate(train_true_labels)
+        train_pred_labels = np.concatenate(train_pred_labels)
+        val_true_labels = np.concatenate(val_true_labels)
+        val_pred_labels = np.concatenate(val_pred_labels)
+
+        # Generate confusion matrices and plots
+        train_conf_matrix = confusion_matrix(train_true_labels, train_pred_labels)
+        val_conf_matrix = confusion_matrix(val_true_labels, val_pred_labels)
+
+        sns.heatmap(train_conf_matrix, annot=True, fmt='d')
+        plt.title('Training Confusion Matrix')
+        plt.show()
+
+        sns.heatmap(val_conf_matrix, annot=True, fmt='d')
+        plt.title('Validation Confusion Matrix')
+        plt.show()
+
     torch.save(model.state_dict(), f'{env}/models/{model_name}.pth')
 
-
-
-class Model(nn.Module):
-    def __init__(self, num_classes=2):
-        super(Model, self).__init__()
-        
-        # Define a convolutional feature extractor
-        self.features = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-        )
-        
-        # Define an adaptive pooling layer to handle variable input sizes
-        self.adaptive_pool = nn.AdaptiveAvgPool2d((10, 10))
-        
-        # Define a fully connected layer for classification
-        self.fc_image = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(256*10*10, 128),
-            nn.ReLU(inplace=True)
-        )
-
-        self.fc_combined = nn.Sequential(
-            nn.Linear(128, 128),
-            nn.ReLU(inplace=True),
-            nn.Linear(128, 64),
-            nn.ReLU(inplace=True),
-            nn.Linear(64, num_classes)
-        )
-
-    def forward(self, image):
-        image_features = self.features(image)
-        image_features = self.adaptive_pool(image_features)
-        image_features = self.fc_image(image_features)
-        
-        output = self.fc_combined(image_features)
-        return output
-    
     
 if __name__ == "__main__":
     
     model_name = 'model_09_02_2023'
-    epochs = 3
+    epochs = 2
     image_size = 512
+    batch_size = 5
     
+    output_size = 1
     
     
     # RESNET Model
-    model = models.resnet34(pretrained=True)
+    model = models.resnet18(pretrained=True)
     num_ftrs = model.fc.in_features
     model.conv1 = torch.nn.Conv2d(3, model.conv1.out_channels, kernel_size=7, stride=2, padding=3, bias=False)
-    model.fc = torch.nn.Linear(num_ftrs, 2)
 
-    #model = Model(num_classes=2)
+    dropout_rate = 0.5  # Set dropout rate
+
+    # Create a new sequential container for the fully connected part
+    fc_layers = nn.Sequential(
+        nn.Dropout(dropout_rate),
+        nn.Linear(num_ftrs, output_size)  # Assuming you're using the model for a 1000-class problem
+    )
+    
+    model.fc = torch.nn.Linear(num_ftrs, output_size)
     model = model.to(device)
     
     #Preparing data
@@ -324,14 +304,14 @@ if __name__ == "__main__":
     # Create datasets
     train_dataset = CASBUSI_Dataset(train_data, f'{cropped_images}/', transform=train_transform)
     val_dataset = CASBUSI_Dataset(val_data, f'{cropped_images}/', transform=val_transform)
-
+    
     # Create data loaders
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=1, shuffle=True, collate_fn=collate_fn, num_workers=8, persistent_workers=True, pin_memory=True)
-    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=1, shuffle=False, collate_fn=collate_fn, num_workers=4, persistent_workers=True, pin_memory=True)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, sampler=sampler, shuffle=True, collate_fn=collate_fn, num_workers=8, persistent_workers=True, pin_memory=True)
+    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=False, collate_fn=collate_fn, num_workers=4, persistent_workers=True, pin_memory=True)
 
     
     os.makedirs(f"{env}/models/", exist_ok=True)
-    model = load_model(model, f"{env}/models/{model_name}.pt")
+    model = load_model(model, f"{env}/models/{model_name}.pth")
     #summary(model, input_size=(1, 1, image_size, image_size))
     print(f'Total model parameters: {sum(p.numel() for p in model.parameters())}')
 
