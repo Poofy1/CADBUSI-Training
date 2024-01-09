796a75ccff1f7f853b9f8fd7f95222d62d05a71d
Poof <poofy38@gmail.com>
2024-01-09 16:22:33 -0600

    ITS2CLR rough draft

--- train_ITS2CLR.py
+++ train_ITS2CLR.py
@@ -14,6 +14,79 @@ device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
     
     
+    
+class ITS2CLR_Dataset(TUD.Dataset):
+    def __init__(self, bags_dict, train=True, save_processed=False, bag_type='all'):
+        self.bags_dict = bags_dict
+        self.save_processed = save_processed
+        self.train = train
+        self.unique_bag_ids = list(bags_dict.keys())
+
+        # Filter bags based on bag_type
+        if bag_type != 'all':
+            self.unique_bag_ids = [bag_id for bag_id in self.unique_bag_ids
+                                if (self.bags_dict[bag_id]['bag_labels'][0] == 1 and bag_type == 'positive') or 
+                                    (self.bags_dict[bag_id]['bag_labels'][0] == 0 and bag_type == 'negative')]
+            
+        # Normalize
+        if train:
+            self.tsfms = T.Compose([
+                T.RandomVerticalFlip(),
+                T.RandomHorizontalFlip(),
+                #T.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0),
+                T.RandomAffine(degrees=(-45, 45), translate=(0.05, 0.05), scale=(1, 1.2),),
+                #HistogramEqualization(),
+                CLAHETransform(),
+                T.ToTensor(),
+                GaussianNoise(mean=0, std=0.015),  # Add slight noise
+                T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+            ])
+        else:
+            self.tsfms = T.Compose([
+                #HistogramEqualization(),
+                CLAHETransform(),
+                T.ToTensor(),
+                T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+            ])
+    
+    def __getitem__(self, index):
+        actual_id = self.unique_bag_ids[index]
+        bag_info = self.bags_dict[actual_id]
+
+        # Extract labels, image file paths, and instance-level labels
+        bag_labels = bag_info['bag_labels']
+        files_this_bag = bag_info['images']
+        instance_labels = bag_info['image_labels']
+
+        # Process images
+        image_data = torch.stack([self.tsfms(Image.open(fn).convert("RGB")) for fn in files_this_bag])
+        image_data = image_data.cuda()  # Move to GPU if CUDA is available
+        
+        # Save processed images if required
+        if self.save_processed:
+            save_folder = os.path.join(env, 'processed_images')  
+            os.makedirs(save_folder, exist_ok=True)
+            for idx, img_tensor in enumerate(image_data):
+                img_save_path = os.path.join(save_folder, f'bag_{actual_id}_img_{idx}.png')
+                img_tensor = unnormalize(img_tensor)
+                img = TF.to_pil_image(img_tensor.cpu().detach())
+                img.save(img_save_path)
+
+        # Convert bag labels list to a tensor
+        bag_labels_tensor = torch.tensor(bag_labels, dtype=torch.float32)
+
+        # Convert instance labels to a tensor, using -1 for None
+        instance_labels_tensors = [torch.tensor(labels, dtype=torch.float32) if labels != [None] else torch.tensor([-1], dtype=torch.float32) for labels in instance_labels]
+
+        return image_data, bag_labels_tensor, instance_labels_tensors, actual_id
+
+    
+    def __len__(self):
+        return len(self.unique_bag_ids)
+    
+    def n_features(self):
+        return self.data.size(1)
+    
 def collate_custom(batch):
     batch_data = []
     batch_bag_labels = []
@@ -34,6 +107,8 @@ def collate_custom(batch):
     out_ids = torch.tensor(batch_ids, dtype=torch.long).cuda()
 
     return batch_data, out_bag_labels, batch_instance_labels, out_ids
+
+
 
 class EmbeddingBagModel(nn.Module):
     
@@ -219,6 +294,31 @@ class SupConLoss(nn.Module):
 
         return loss
 
+def spl_scheduler(current_epoch, total_epochs, initial_ratio, final_ratio):
+    if current_epoch < warmup_epochs:
+        return initial_ratio
+    else:
+        return initial_ratio + (final_ratio - initial_ratio) * (current_epoch - warmup_epochs) / (total_epochs - warmup_epochs)
+
+def combine_pos_neg_data(pos_data, neg_data, pos_batch_size, neg_batch_size):
+    if pos_batch_size == 0:
+        return neg_data
+    elif neg_batch_size == 0:
+        return pos_data
+    
+    # Unpack data from positive and negative batches
+    pos_xb, pos_yb, pos_instance_yb, pos_id = pos_data
+    neg_xb, neg_yb, neg_instance_yb, neg_id = neg_data
+
+    # Combine positive and negative data
+    combined_xb = pos_xb + neg_xb
+    combined_yb = torch.cat([pos_yb, neg_yb])
+    combined_instance_yb = pos_instance_yb + neg_instance_yb
+    combined_id = pos_id + neg_id
+
+    return combined_xb, combined_yb, combined_instance_yb, combined_id
+
+
 
 def default_train():
     global val_loss_best
@@ -304,13 +404,18 @@ if __name__ == '__main__':
     dataset_name = 'export_12_26_2023'
     label_columns = ['Has_Malignant']
     instance_columns = [] #['Reject Image', 'Only Normal Tissue', 'Cyst Lesion Present', 'Benign Lesion Present', 'Malignant Lesion Present']
-    feature_extractor_train_count = 5
     img_size = 350
     batch_size = 5
     min_bag_size = 2
     max_bag_size = 20
-    epochs = 500
     lr = 0.001
+    
+    #ITS2CLR Config
+    feature_extractor_train_count = 5
+    initial_ratio = 0
+    final_ratio = .5
+    total_epochs = 200
+    warmup_epochs = 1
 
     # Paths
     export_location = f'D:/DATA/CASBUSI/exports/{dataset_name}/'
@@ -325,14 +430,16 @@ if __name__ == '__main__':
 
     print("Training Data...")
     # Create datasets
-    dataset_train = TUD.Subset(BagOfImagesDataset(bags_train, save_processed=False),list(range(0,100)))
-    dataset_val = TUD.Subset(BagOfImagesDataset(bags_val, save_processed=False),list(range(0,100)))
-    #dataset_train = BagOfImagesDataset(bags_train, save_processed=False)
-    #dataset_val = BagOfImagesDataset(bags_val, train=False)
+    #dataset_train = TUD.Subset(BagOfImagesDataset(bags_train, save_processed=False),list(range(0,100)))
+    #dataset_val = TUD.Subset(BagOfImagesDataset(bags_val, save_processed=False),list(range(0,100)))
+    positive_train_dataset = ITS2CLR_Dataset(bags_train, train=True, save_processed=False, bag_type='positive')
+    negative_train_dataset = ITS2CLR_Dataset(bags_train, train=True, save_processed=False, bag_type='negative')
+    dataset_val = BagOfImagesDataset(bags_val, train=False)
 
             
     # Create data loaders
-    train_dl =  TUD.DataLoader(dataset_train, batch_size=batch_size, collate_fn = collate_custom, drop_last=True, shuffle = True)
+    pos_train_dl =  TUD.DataLoader(positive_train_dataset, batch_size=batch_size, collate_fn = collate_custom, drop_last=True, shuffle = True)
+    neg_train_dl =  TUD.DataLoader(negative_train_dataset, batch_size=batch_size, collate_fn = collate_custom, drop_last=True, shuffle = True)
     val_dl =    TUD.DataLoader(dataset_val, batch_size=batch_size, collate_fn = collate_custom, drop_last=True)
 
     
@@ -351,7 +458,6 @@ if __name__ == '__main__':
     optimizer = Adam(bagmodel.parameters(), lr=lr)
     loss_func = nn.BCELoss()
     supcon_loss = SupConLoss(temperature=0.07, pair_mode = 2, contrast_mode='all', base_temperature=0.07)
-    #genSCL_loss = GeneralizedSupervisedContrastiveLoss(temperature=0.07)
     train_losses_over_epochs = []
     valid_losses_over_epochs = []
     epoch_start = 0
@@ -382,7 +488,7 @@ if __name__ == '__main__':
 
 
     # Training loop
-    for epoch in range(epoch_start, epochs):
+    for epoch in range(epoch_start, total_epochs):
         if epoch % (1 + feature_extractor_train_count) == 0:
             
             print('Training Default')
@@ -395,13 +501,36 @@ if __name__ == '__main__':
                 param.requires_grad = False
             for param in encoder.parameters():
                 param.requires_grad = True
-                
+            
+
+            # Get difficualy ratio
+            current_ratio = spl_scheduler(epoch, total_epochs, initial_ratio, final_ratio)
+            pos_batch_size = int(current_ratio * batch_size)
+            neg_batch_size = batch_size - pos_batch_size
+
+            # Iterator for positive and negative data loaders
+            pos_data_iter = iter(pos_train_dl)
+            neg_data_iter = iter(neg_train_dl)
+
+            # Determine the total number of batches
+            total_batches = max(len(pos_train_dl), len(neg_train_dl))
+            
             epoch_loss = 0.0
 
             # Iterate over the training data
-            for (data, yb, instance_yb, id) in tqdm(train_dl, total=len(train_dl)):
-                xb, yb = data, yb.cuda()
-                yb = yb.view(-1)
+            for _ in tqdm(range(total_batches)):
+                # Fetch batches from positive and negative data loaders
+                pos_data = next(pos_data_iter, None)
+                neg_data = next(neg_data_iter, None)
+
+                if pos_data is None or neg_data is None:
+                    break  # Break if either data loader is exhausted
+
+                # Combine positive and negative batches
+                combined_data = combine_pos_neg_data(pos_data, neg_data, pos_batch_size, neg_batch_size)
+                xb, yb, instance_yb, id = combined_data
+            
+                yb = yb.cuda().view(-1)
                 optimizer.zero_grad()
 
                 # Generate pseudo labels from attention scores
@@ -447,7 +576,7 @@ if __name__ == '__main__':
                 optimizer.step()
                 
             # Calculate the average loss for the epoch
-            epoch_loss /= batch_size
+            epoch_loss /= total_batches
             print(f'Loss: {epoch_loss:3f}')
 
             # After the contrastive update, unfreeze the aggregator and encoder
