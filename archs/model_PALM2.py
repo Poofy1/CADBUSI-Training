3d62c7f02cda7f1fec43dd94d12fcb2b9b487a04
Poof <poofy38@gmail.com>
2024-06-28 19:36:41 -0500

    PALM testing

--- archs/model_PALM2.py
+++ archs/model_PALM2.py
@@ -6,62 +6,80 @@ from archs.backbone import create_timm_body
 from torchvision.models import efficientnet_b3, EfficientNet_B3_Weights
 
 class Embeddingmodel(nn.Module):
-    def __init__(self, encoder, nf, num_classes=1, efficient_net=False):
+    def __init__(self, encoder, nf, num_classes=1, feat_dim=128, efficient_net=False):
         super(Embeddingmodel, self).__init__()
         self.encoder = encoder
         self.efficient_net = efficient_net
         self.num_classes = num_classes
         self.nf = nf
-        #self.aggregator = Saliency_Classifier(nf=self.nf, num_classes=num_classes)
-        self.aggregator = Linear_Classifier2(nf=self.nf, num_classes=num_classes)
+
+        self.aggregator = Linear_Classifier(nf=self.nf, num_classes=num_classes)
         self.projector = nn.Sequential(
             nn.Linear(nf, 512),
             nn.ReLU(inplace=True),
-            nn.Linear(512, 256)
+            nn.Linear(512, feat_dim)
         )
+        
+        self.ins_classifier = nn.Sequential(
+            nn.Linear(nf, 512),
+            nn.ReLU(inplace=True),
+            nn.Linear(512, num_classes),
+            nn.Sigmoid()
+        )
+        
+        """self.saliency_layer = nn.Sequential(        
+            nn.Conv2d(nf, num_classes, (1,1), bias = False),
+            nn.Sigmoid()
+        )"""
+        
         self.adaptive_avg_pool = nn.AdaptiveAvgPool2d((1, 1))
         print(f'Feature Map Size: {nf}')
 
     def forward(self, input, projector=False, pred_on = False):
-        num_bags = len(input) # input = [bag #, image #, channel, height, width]
-        all_images = torch.cat(input, dim=0).cuda()  # Concatenate all bags into a single tensor for batch processing
+        if pred_on:
+            num_bags = len(input) # input = [bag #, image #, channel, height, width]
+            all_images = torch.cat(input, dim=0).cuda()  # Concatenate all bags into a single tensor for batch processing
+        else:
+            all_images = input
 
         # Calculate the embeddings for all images in one go
         feat = self.encoder(all_images)
-        if not self.efficient_net:
-            # Max pooling
-            #feat = torch.max(feat, dim=2).values
-            #feat = torch.max(feat, dim=2).values
-            
-            # Adaptive average pooling
-            feat = self.adaptive_avg_pool(feat).squeeze()
-            
-            # Global average pooling
-            #feat = torch.mean(feat, dim=(2, 3))
+        feat_pool = self.adaptive_avg_pool(feat).squeeze()
+        
+        # SALIENCY CLASS
+        """saliency_maps = self.saliency_layer(feat)  # Generate saliency maps using a convolutional layer
+        map_flatten = saliency_maps.flatten(start_dim=-2, end_dim=-1) 
+        selected_area = map_flatten.topk(3, dim=2)[0]
+        instance_predictions = selected_area.mean(dim=2).squeeze()  # Calculate the mean of the selected patches for instance predictions
+        """
+        
+        
+        # INSTANCE CLASS
+        instance_predictions = self.ins_classifier(feat_pool)
+        feat = feat_pool
 
+        bag_pred = None
+        bag_instance_predictions = None
         if pred_on:
             # Split the embeddings back into per-bag embeddings
             split_sizes = [bag.size(0) for bag in input]
             h_per_bag = torch.split(feat, split_sizes, dim=0)
-            logits = torch.empty(num_bags, self.num_classes).cuda()
-            yhat_instances = []
-            for i, h in enumerate(h_per_bag):
-                # Receive four values from the aggregator
-                yhat_bag, yhat_ins = self.aggregator(h)
-                logits[i] = yhat_bag
-                yhat_instances.append(yhat_ins)
-        else:
-            logits = None
-            yhat_instances = None
-            
+            y_hat_per_bag = torch.split(instance_predictions, split_sizes, dim=0)
+            bag_pred = torch.empty(num_bags, self.num_classes).cuda()
+            bag_instance_predictions = []
+            for i, (h, y_h) in enumerate(zip(h_per_bag, y_hat_per_bag)):
+                # Pass both h and y_hat to the aggregator
+                yhat_bag, yhat_ins = self.aggregator(h, y_h)
+                bag_pred[i] = yhat_bag
+                bag_instance_predictions.append(yhat_ins) 
+        
+        proj = None
         if projector:
-            #feat = self.adaptive_avg_pool(feat).squeeze()
-            feat = self.projector(feat)
-            feat = F.normalize(feat, dim=1)
+            proj = self.projector(feat_pool)
+            proj = F.normalize(proj, dim=1)
             
 
-        return logits, yhat_instances, feat
-
+        return bag_pred, bag_instance_predictions, instance_predictions.squeeze(), proj
 
 
 class Linear_Classifier(nn.Module):
@@ -84,12 +102,6 @@ class Linear_Classifier(nn.Module):
             nn.Linear(L, 1),
         )
         
-        self.fc = nn.Sequential(
-            nn.Linear(nf, num_classes),
-            nn.Sigmoid()
-        )
-        
-        
     def reset_parameters(self):
         # Reset the parameters of all the submodules in the Linear_Classifier
         for module in self.modules():
@@ -97,57 +109,7 @@ class Linear_Classifier(nn.Module):
                 module.reset_parameters()
         
         
-    def forward(self, v):
-        
-        A_V = self.attention_V(v)  # KxL
-        A_U = self.attention_U(v)  # KxL
-        instance_scores = self.attention_W(A_V * A_U) # element wise multiplication # KxATTENTION_BRANCHES
-        A = torch.transpose(instance_scores, 1, 0)  # ATTENTION_BRANCHESxK
-        A = F.softmax(A, dim=1)  # softmax over K
-
-        Z = torch.mm(A, v)  # ATTENTION_BRANCHESxM
-
-        Y_prob = self.fc(Z)
-        
-        instance_scores = torch.sigmoid(instance_scores.squeeze())
-        
-        return Y_prob, instance_scores
-
-
-class Linear_Classifier2(nn.Module):
-    """Linear classifier"""
-    def __init__(self, nf, num_classes=1, L=256):
-        super(Linear_Classifier2, self).__init__()
-        self.fc = nn.Linear(nf, num_classes)
-        
-        
-        # Attention mechanism components
-        self.attention_V = nn.Sequential(
-            nn.Linear(nf, L),
-            nn.Tanh()
-        )
-        self.attention_U = nn.Sequential(
-            nn.Linear(nf, L),
-            nn.Sigmoid()
-        )
-        self.attention_W = nn.Sequential(
-            nn.Linear(L, 1),
-        )
-        
-        self.fc = nn.Sequential(
-            nn.Linear(nf, num_classes),
-            nn.Sigmoid()
-        )
-        
-        
-    def reset_parameters(self):
-        # Reset the parameters of all the submodules in the Linear_Classifier
-        for module in self.modules():
-            if isinstance(module, nn.Linear):
-                module.reset_parameters()
-        
-        
-    def forward(self, v):
+    def forward(self, v, y_hat):
         
         A_V = self.attention_V(v)  # KxL
         A_U = self.attention_U(v)  # KxL
@@ -155,26 +117,22 @@ class Linear_Classifier2(nn.Module):
         A = torch.transpose(instance_scores, 1, 0)  # ATTENTION_BRANCHESxK
         A = F.softmax(A, dim=1)  # softmax over K
         
-        # Apply fc layer to feat-level features
-        feat_predictions = self.fc(v)  # KxC
-        
+
         # Aggregate instance-level predictions
-        Y_prob = torch.mm(A, feat_predictions)  # ATTENTION_BRANCHESxC
+        Y_prob = torch.mm(A, y_hat)  # ATTENTION_BRANCHESxC
 
         instance_scores = torch.sigmoid(instance_scores.squeeze())
         return Y_prob, instance_scores
+    
+    
     
 class Saliency_Classifier(nn.Module):
     """Linear classifier"""
     def __init__(self, nf, num_classes=1, L=256):
         super(Saliency_Classifier, self).__init__()
         self.fc = nn.Linear(nf, num_classes)
-        self.pool_patches = 3
         
-        self.saliency_layer = nn.Sequential(        
-            nn.Conv2d(nf, num_classes, (1,1), bias = False),
-            nn.Sigmoid()
-        )
+        
         
         # Attention mechanism components
         self.attention_V = nn.Sequential(
@@ -197,15 +155,8 @@ class Saliency_Classifier(nn.Module):
                 module.reset_parameters()
         
         
-    def forward(self, h):
-
-        saliency_maps = self.saliency_layer(h)  # Generate saliency maps using a convolutional layer
-        map_flatten = saliency_maps.flatten(start_dim=-2, end_dim=-1) 
+    def forward(self, h, yhat_instance):
         
-        # Select top patches based on saliency
-        selected_area = map_flatten.topk(self.pool_patches, dim=2)[0]
-        yhat_instance = selected_area.mean(dim=2).squeeze()  # Calculate the mean of the selected patches for instance predictions
-
         # Gated-attention mechanism
         v = torch.max(h, dim=2).values  # Max pooling across one dimension
         v = torch.max(v, dim=2).values  # Max pooling across the remaining spatial dimension
--- data/create_imagenet.py
+++ data/create_imagenet.py
@@ -6,7 +6,8 @@ import random
 import shutil
 
 output_dir = "D:\DATA\CASBUSI\exports"
-positive_percentage = 0.2  # Percentage of positive instances to include 
+export_name = "imagenette2_insane"
+positive_percentage = 0.1  # Percentage of positive instances to include 
 
 
 if not os.path.exists(f'{output_dir}/imagenette2.tgz'):
@@ -16,26 +17,28 @@ if not os.path.exists(f'{output_dir}/imagenette2.tgz'):
     with open(f'{output_dir}/imagenette2.tgz', 'wb') as file:
         shutil.copyfileobj(response.raw, file)
 
-if not os.path.exists(f'{output_dir}/imagenette2'):
+if not os.path.exists(f'{output_dir}/{export_name}'):
     # Extract the dataset
     with tarfile.open(f'{output_dir}/imagenette2.tgz', 'r:gz') as tar_ref:
-        tar_ref.extractall(f'{output_dir}')
+        tar_ref.extractall(f'{output_dir}/{export_name}')
 
 # Delete the .tgz file
 #os.remove(f'{output_dir}/imagenette2.tgz')
 
 # Set up the directories
-images_dir = f'{output_dir}/imagenette2/images'
+images_dir = f'{output_dir}/{export_name}/images'
+train_dir = f'{output_dir}/{export_name}/train'
 os.makedirs(images_dir, exist_ok=True)
+os.makedirs(train_dir, exist_ok=True)
 
 # Create the intermediate CSV file to store image labels
-with open(f'{output_dir}/imagenette2/image_labels.csv', 'w', newline='') as file:
+with open(f'{output_dir}/{export_name}/image_labels.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerow(['Image', 'Label'])
     
     # Move all images to the images directory and write image labels to the CSV file
-    for label_dir in os.listdir(f'{output_dir}/imagenette2/train'):
-        label_path = os.path.join(f'{output_dir}/imagenette2/train', label_dir)
+    for label_dir in os.listdir(f'{output_dir}/{export_name}/imagenette2/train'):
+        label_path = os.path.join(f'{output_dir}/{export_name}/imagenette2/train', label_dir)
         if os.path.isdir(label_path):
             for file in os.listdir(label_path):
                 if file.endswith('.JPEG'):
@@ -46,7 +49,7 @@ with open(f'{output_dir}/imagenette2/image_labels.csv', 'w', newline='') as file
 
 # Read the image labels from the intermediate CSV file
 image_labels = {}
-with open(f'{output_dir}/imagenette2/image_labels.csv', 'r') as file:
+with open(f'{output_dir}/{export_name}/image_labels.csv', 'r') as file:
     reader = csv.reader(file)
     next(reader)  # Skip the header row
     for row in reader:
@@ -82,13 +85,13 @@ for image_name, label in image_labels.items():
         instance_data.append([accession_number, int(has_fish), image_name])
 
 # Write the train.csv file
-with open(f'{output_dir}/imagenette2/TrainData.csv', 'w', newline='') as file:
+with open(f'{output_dir}/{export_name}/TrainData.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerow(['Valid', 'Images', 'ID', 'Has_Fish', 'Accession_Number'])
     writer.writerows(train_data)
 
 # Write the InstanceData.csv file
-with open(f'{output_dir}/imagenette2/InstanceData.csv', 'w', newline='') as file:
+with open(f'{output_dir}/{export_name}/InstanceData.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerow(['Accession_Number', 'Has_Fish', 'ImageName'])
     writer.writerows(instance_data)
