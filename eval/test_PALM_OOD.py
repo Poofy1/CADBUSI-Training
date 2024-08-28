011176aa7d9c299876ca3939afc8d75a360f605e
Poof <poofy38@gmail.com>
2024-08-28 15:44:11 -0500

    Dataloader simlification

--- eval/test_PALM_OOD.py
+++ eval/test_PALM_OOD.py
@@ -19,92 +19,9 @@ from util.sudo_labels import *
 from loss.palm import PALM
 from archs.save_arch import *
 from archs.model_PALM2_solo import *
+from data.bag_loader import *
+from data.instance_loader import *
 
-class Instance_Dataset_with_IDs(TUD.Dataset):
-    def __init__(self, bags_dict, selection_mask, transform=None, warmup=True):
-        self.transform = transform
-        self.warmup = warmup
-
-        self.images = []
-        self.final_labels = []
-        self.bag_ids = []
-        self.image_indices = []
-
-        self.unique_bag_ids = list(bags_dict.keys())
-
-        for bag_id, bag_info in bags_dict.items():
-            images = bag_info['images']
-            image_labels = bag_info['image_labels']
-            bag_label = bag_info['bag_labels'][0]  # Assuming each bag has a single label
-            
-            bag_id_key = bag_id.item() if isinstance(bag_id, torch.Tensor) else bag_id
-            
-            if bag_id_key in selection_mask:
-                selection_mask_labels, _ = selection_mask[bag_id_key]
-            else: 
-                selection_mask_labels = None
-
-            for idx, (img, label) in enumerate(zip(images, image_labels)):
-                image_label = None
-                
-                if self.warmup:
-                    # Only include confident instances (selection_mask) or negative bags or instance labels
-                    if label[0] is not None:
-                        image_label = label[0]
-                    elif bag_label == 0:
-                        image_label = 0
-                    elif selection_mask_labels is not None and selection_mask_labels[idx] != -1:
-                        image_label = selection_mask_labels[idx]
-                else:
-                    # Return all images with unknown possibility 
-                    if label[0] is not None:
-                        image_label = label[0]
-                    elif bag_label == 0:
-                        image_label = 0
-                    elif selection_mask_labels is not None and selection_mask_labels[idx] != -1:
-                        image_label = selection_mask_labels[idx]
-                    else:
-                        image_label = -1
-                
-                if image_label is not None:
-                    self.images.append(img)
-                    self.final_labels.append(image_label)
-                    self.bag_ids.append(bag_id_key)
-                    self.image_indices.append(idx)
-
-    def __getitem__(self, index):
-        img_path = self.images[index]
-        instance_label = self.final_labels[index]
-        bag_id = self.bag_ids[index]
-        image_index = self.image_indices[index]
-        
-        img = cv2.imread(img_path)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # OpenCV loads in BGR, so convert to RGB
-        img = Image.fromarray(img)
-        image_data = self.transform(img)
-
-        return image_data, instance_label, bag_id, image_index
-    
-    def __len__(self):
-        return len(self.images)
-
-def collate_instance(batch):
-    batch_data = []
-    batch_labels = []
-    batch_bag_ids = []
-    batch_image_indices = []
-
-    for image_data, bag_label, bag_id, image_index in batch:
-        batch_data.append(image_data)
-        batch_labels.append(bag_label)
-        batch_bag_ids.append(bag_id)
-        batch_image_indices.append(image_index)
-
-    # Stack the images and labels
-    batch_data = torch.stack(batch_data)
-    batch_labels = torch.tensor(batch_labels, dtype=torch.long)
-
-    return batch_data, batch_labels, batch_bag_ids, batch_image_indices
 
 
 def test_model_and_collect_distances(model, palm, bag_dataloader, instance_dataloader, device):
@@ -195,7 +112,7 @@ def run_test(dataset_name, label_columns, instance_columns, config):
     bag_dataset_test = BagOfImagesDataset(bags_test, transform=test_transform, save_processed=False)
     bag_dataloader_test = TUD.DataLoader(bag_dataset_test, batch_size=bag_batch_size, collate_fn=collate_bag, shuffle=False)
 
-    instance_dataset_test = Instance_Dataset_with_IDs(bags_test, [], transform=test_transform, warmup=True)
+    instance_dataset_test = Instance_Dataset(bags_test, [], transform=test_transform, warmup=True)
     instance_dataloader_test = TUD.DataLoader(instance_dataset_test, batch_size=instance_batch_size, collate_fn=collate_instance, shuffle=False)
 
     # Load the trained model
